4a0a6f5f592ca9ef4030bc689899bebabca647ed
Luke F <luke@lavnercampsandprograms.com>
2024-06-24 13:15:23 -0400

    Finally Launch Powershell Scripts from python

--- src/amongussteamscript/app.py
+++ src/amongussteamscript/app.py
@@ -3,31 +3,37 @@ Automates the reinstallation of Among Us
 """
 
 import subprocess
-import pyautogui, time
+import pyautogui, time, os
 
 def runPowershell(script):
-    POWERSHELL_PATH = "C:\system32\WindowsPowershell\v5.1\powershell.exe"
+    POWERSHELL_PATH = 'C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe'
     commandline_options = [POWERSHELL_PATH, script]
-    process_result = subprocess.run(commandline_options, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+    process_result = subprocess.run(commandline_options, universal_newlines = True)
     print(process_result.returncode)
 def main():
     screenWidth, screenHeight = pyautogui.size()
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    runPowershell(os.getcwd() + '\\resources\\fixBlackScreen.ps1')
+    #'C:\\AmongUsInstallation\\src\\amongussteamscript\\resources\\fixBlackScreen.ps1'
     pyautogui.alert(text='Don\'t touch your keyboard or mouse.', title='', button='OK') 
     pyautogui.keyDown("win")
     pyautogui.press("up")
     pyautogui.keyUp("win")
-    pyautogui.click(x=197, y=58)
+    pyautogui.click(x=217, y=67)
     time.sleep(1)
-    pyautogui.click(x=100,y=250)
+    pyautogui.click(x=222,y=306)
     time.sleep(1)
-    pyautogui.click(button='right',x=100,y=250)
+    pyautogui.click(button='right',x=222,y=306)
     time.sleep(1)
-    pyautogui.moveTo(230,365)
+    pyautogui.moveTo(346,459)
     time.sleep(1)
-    pyautogui.click(x=349,y=493)
+    pyautogui.click(x=482,y=628)
     time.sleep(1)
-    pyautogui.click(x=1057, y=583)
+    pyautogui.click(x=1085, y=602)
     time.sleep(1)
-    pyautogui.click(x=395, y=425)
+    pyautogui.click(x=499, y=534)
     time.sleep(1)
-    pyautogui.click(x=902, y=699)
+    pyautogui.click(x=865, y=735)
+    #runPowershell(os.cwd() + '\\resources\\moveManifest.ps1')
